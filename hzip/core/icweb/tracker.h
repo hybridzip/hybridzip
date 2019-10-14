00e36b099f28a0f0492549a256de6da92bb1cdf0
VishaalSelvaraj2000 <vishaals2000@gmail.com>
2019-10-14 11:41:53 +0530

    add compression tracker class

--- hzip/core/icweb/tracker.h
+++ hzip/core/icweb/tracker.h
@@ -1,8 +1,21 @@
 #ifndef HYBRIDZIP_INSANE_CONTEXT_WEB_TRACKER_H
 #define HYBRIDZIP_INSANE_CONTEXT_WEB_TRACKER_H
 
+#include "instance.h"
+#include "../../other/platform.h"
+
+
 namespace icweb {
-    class tracker{};
+    class cprs_tracker {
+    private:
+        float target;
+        float avg;
+        float* buffer;
+    public:
+        cprs_tracker();
+        void track(icweb::instance* inst);
+        void feed(HZIP_UINT nbits);
+    };
 }
 
 #endif

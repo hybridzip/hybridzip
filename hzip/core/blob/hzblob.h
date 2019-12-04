646243d66844e5c7e885d90eaf3d96d1547e8606
supercmmetry <supercmmetry@VISHAAL-OMEN.localdomain>
2019-12-04 22:14:19 +0530

    Implement multi-threaded hzrans encoder.

---
+++ hzip/core/blob/hzblob.h
@@ -0,0 +1,17 @@
+#ifndef HYBRIDZIP_HZBLOB_H
+#define HYBRIDZIP_HZBLOB_H
+
+#include <cstdint>
+#include "../../other/platform.h"
+
+struct hzrblob_t {
+    uint32_t *data;
+    uint64_t size;
+};
+
+struct hzrblob_set{
+    hzrblob_t *blobs;
+    uint32_t count;
+};
+
+#endif

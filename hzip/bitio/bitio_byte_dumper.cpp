05b4d7684ed55dbfe5c0d3a96c7c1593c04eeb43
supercmmetry <vishaals2000@gmail.com>
2019-12-25 11:49:21 +0530

    Add batch-processing for encoder.
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

---
+++ hzip/bitio/bitio_byte_dumper.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Vishaal Selvaraj on 09-12-2019.
+//
+#include "bitio.h"
+
+using namespace bitio;
+
+bitio_byte_stream::bitio_byte_stream(char *filename, bitio::access_enum access, uint64_t buffer_size) {
+    out = fopen
+}
+
+

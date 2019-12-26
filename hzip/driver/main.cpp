dd229ab6c66a1fbd733ed88412f35725e1d7c0f5
supercmmetry <vishaals2000@gmail.com>
2019-12-26 13:42:42 +0530

    Resolve merge conflict

--- hzip/driver/main.cpp
+++ hzip/driver/main.cpp
@@ -2,7 +2,9 @@
 #include <random>
 #include <chrono>
 #include <cmath>
+#include <cstring>
 #include <cassert>
+
 
 #define HZRANS_USE_AVX 1
 
@@ -13,6 +15,8 @@
 
 
 #define SIZE 1048576
+#define FILENAME "/run/media/supercmmetry/dickens/dickens"
+#define OFILENAME "/run/media/supercmmetry/dickens/dickens.hz"
 #define BUFFER_SIZE 1ull << 20ull
 
 int main() {
@@ -20,7 +24,7 @@ int main() {
 
     //read 1KB from dickens...
     FILE *fp;
-    fp = fopen("E:/dickens/dickens", "rb");
+    fp = fopen(FILENAME, "rb");
     uint8_t *array = new uint8_t[SIZE];
     fread(array, 1, SIZE, fp);
 
@@ -34,7 +38,7 @@ int main() {
 
 
     auto clock = std::chrono::high_resolution_clock();
-    bitio::bitio_stream bstream(_strdup("E:/dickens.hzbs"), bitio::WRITE, 1024);
+    bitio::bitio_stream bstream(strdup(OFILENAME), bitio::WRITE, 1024);
 
     auto start = clock.now();
     auto set = hzmbb.run();
@@ -47,7 +51,7 @@ int main() {
 
     std::cout << "\n\nTime taken (encoder): " << (clock.now() - start).count() << " ns" << std::endl;
 
-    bitio::bitio_stream rstream(_strdup("E:/dickens.hzbs"), bitio::READ, 1024);
+    bitio::bitio_stream rstream(strdup("E:/dickens.hzbs"), bitio::READ, 1024);
     hzBlobUnpacker unpacker;
     unpacker.unpack(rstream);
 

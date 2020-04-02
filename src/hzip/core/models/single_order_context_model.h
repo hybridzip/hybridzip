8ada63a55a5a58e0dc9198b2b79b7838892c9983
supercmmetry <vishaals2000@gmail.com>
2020-04-02 06:57:08 +0530

    model: implement single context model

---
+++ src/hzip/core/models/single_order_context_model.h
@@ -0,0 +1,45 @@
+#ifndef HYBRIDZIP_SINGLE_ORDER_CONTEXT_MODEL_H
+#define HYBRIDZIP_SINGLE_ORDER_CONTEXT_MODEL_H
+
+#include <cstdint>
+
+class SingleOrderContextModel {
+private:
+    uint64_t **context_map;
+    uint64_t prev_symbol;
+    int alphabet_size;
+    bool has_started = false;
+public:
+    SingleOrderContextModel(int alphabet_size) {
+        context_map = new uint64_t*[alphabet_size];
+        for (int i = 0; i < alphabet_size; i++) {
+            context_map[i] = new uint64_t[alphabet_size];
+            for (int j = 0; j < alphabet_size; j++) {
+                context_map[i][j] = 0;
+            }
+        }
+        prev_symbol = 0;
+        this->alphabet_size = alphabet_size;
+    }
+
+    uint64_t* predict(uint64_t symbol) {
+        return context_map[symbol];
+    }
+
+    void update(uint64_t symbol) {
+        if (has_started) {
+            context_map[prev_symbol][symbol]++;
+        }
+        prev_symbol = symbol;
+        has_started = true;
+    }
+
+    ~SingleOrderContextModel() {
+        for (int i = 0; i < alphabet_size; i++) {
+            free(context_map[i]);
+        }
+        free(context_map);
+    }
+};
+
+#endif

615836c30b138289b77cdf84c80f57070dbd7ecf
supercmmetry <vishaals2000@gmail.com>
2020-07-10 18:28:16 +0530

    hzip: commit pending changes
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

---
+++ src/hzip/utils/stack.h
@@ -0,0 +1,8 @@
+//
+// Created by supercmmetry on 7/8/20.
+//
+
+#ifndef HYBRIDZIP_STACK_H
+#define HYBRIDZIP_STACK_H
+
+#endif //HYBRIDZIP_STACK_H
---
+++ src/terminal.h
@@ -0,0 +1,8 @@
+//
+// Created by supercmmetry on 7/9/20.
+//
+
+#ifndef HYBRIDZIP_TERMINAL_H
+#define HYBRIDZIP_TERMINAL_H
+
+#endif //HYBRIDZIP_TERMINAL_H

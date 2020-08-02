8563e909ceaefe547d7bc24bb852d612a4185c91
supercmmetry <vishaals2000@gmail.com>
2020-08-02 15:36:25 +0530

    bitio: Use seek_to() feature
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- src/hzip/archiver/archive.h
+++ src/hzip/archiver/archive.h
@@ -1,11 +1,55 @@
 #ifndef HYBRIDZIP_ARCHIVE_H
 #define HYBRIDZIP_ARCHIVE_H
 
+#include <unordered_map>
 #include <bitio/bitio.h>
+#include <hzip/core/blob/hzblob.h>
+
+enum hza_marker {
+    EMPTY = 0x0,
+    METADATA = 0x1,
+    JOURNAL = 0x2,
+    BLOB = 0x3
+};
+
+enum hza_jtask {
+    WRITE = 0x0,
+    DEFRAG = 0x1
+};
+
+
+struct hza_file {
+    uint64_t *blob_ids;
+    uint64_t blob_count;
+    //todo: Add file information.
+};
+
+struct hza_journal_entry {
+    uint64_t obj_sof;
+    uint8_t *data;
+};
+
+struct hza_block_info {
+    hza_marker marker;
+    uint64_t sof;
+    uint64_t size;
+};
+
+struct hza_metadata_file {
+    hza_file file;
+    hza_block_info info;
+};
+
+struct hza_metadata {
+    std::string version;
+    std::unordered_map<std::string, hza_metadata_file> file_map;
+};
 
 class hz_archive {
 private:
     bitio::stream *stream;
+
+    void hza_blob_write(hza_block_info info, hzblob_t *blob);
 
 public:
 

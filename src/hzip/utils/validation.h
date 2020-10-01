53ac1091d3879d23d702b64e7c8ea3d62ca01d56
supercmmetry <vishaals2000@gmail.com>
2020-10-01 13:28:38 +0530

    utils: Add more restrictive validation
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- src/hzip/utils/validation.h
+++ src/hzip/utils/validation.h
@@ -8,6 +8,22 @@ inline void hz_validate_path(const std::string &path) {
     if (!path.starts_with('/')) {
         throw UtilErrors::ValidationError("Path must start with '/'");
     }
+
+    for (char c : path) {
+        if (c == '\0') {
+            throw UtilErrors::ValidationError("Path must not contain null values");
+        } else if (c == '\\') {
+            throw UtilErrors::ValidationError("Path must not contain back slash");
+        } else if (c == ' ') {
+            throw UtilErrors::ValidationError("Path must not contain spaces");
+        }
+    }
+
+    for (int i = 0; i < path.length() - 1; i++) {
+        if (path[i] == '/' && path[i + 1] == '/') {
+            throw UtilErrors::ValidationError("Path must not contain repeated forward slashes");
+        }
+    }
 }
 
 #endif

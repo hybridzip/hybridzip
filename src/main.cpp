31674441607914359b73d22732135416ae270a15
supercmmetry <vishaals2000@gmail.com>
2020-10-06 07:21:52 +0530

    hzip: Report missing vars in env
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,23 @@ void set_unhandled_exception_handler() {
     });
 }
 
+void check_env(cpp_dotenv::dotenv &dotenv) {
+    const char *required_vars[] = {
+            "HZIP_API_THREADS",
+            "HZIP_PROCESSOR_THREADS",
+            "HZIP_API_KEY",
+            "HZIP_API_TIMEOUT",
+            "HZIP_API_PORT"
+    };
+
+    for (auto var : required_vars) {
+        if (dotenv[var].empty()) {
+            LOG_F(ERROR, "hzip: %s was not set in environment", var);
+            exit(0);
+        }
+    }
+}
+
 int main(int argc, const char **argv) {
     setup_logger();
     set_unhandled_exception_handler();
@@ -25,14 +42,16 @@ int main(int argc, const char **argv) {
     auto mgr = new rainman::memmgr;
 
     cpp_dotenv::env.load_dotenv();
-    auto& dotenv = cpp_dotenv::env;
+    auto &dotenv = cpp_dotenv::env;
+
+    check_env(dotenv);
 
     hz_api api;
     rinitfrom(mgr, api);
 
     api.limit(std::stoi(dotenv["HZIP_API_THREADS"]))
-    ->process(std::stoi(dotenv["HZIP_PROCESSOR_THREADS"]))
-    ->protect(dotenv["HZIP_API_KEY"])
-    ->timeout(timeval{.tv_sec=std::stoi(dotenv["HZIP_API_TIMEOUT"])})
-    ->start("127.0.0.1", std::stoi(dotenv["HZIP_API_PORT"]));
+            ->process(std::stoi(dotenv["HZIP_PROCESSOR_THREADS"]))
+            ->protect(dotenv["HZIP_API_KEY"])
+            ->timeout(timeval{.tv_sec=std::stoi(dotenv["HZIP_API_TIMEOUT"])})
+            ->start("127.0.0.1", std::stoi(dotenv["HZIP_API_PORT"]));
 }

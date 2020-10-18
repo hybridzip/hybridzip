d488b0ccf7b2928e3f5e991a1fe381834d61a466
supercmmetry <vishaals2000@gmail.com>
2020-10-18 18:52:10 +0530

    api: Ignore error return on root exception
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- src/hzip/api/api.cpp
+++ src/hzip/api/api.cpp
@@ -86,7 +86,6 @@ void hz_api_instance::start() {
             }
         } catch (std::exception &e) {
             HZAPI_LOG(ERROR, e.what());
-            error(e.what());
             end();
         }
 

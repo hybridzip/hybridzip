c452da84e157e15f58c6485f9f40d590d636420e
supercmmetry <vishaals2000@gmail.com>
2020-10-30 11:33:44 +0530

    scripts: Write packaging scripts
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

---
+++ scripts/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+cp ./bin /usr/bin
+cp ./lib /usr/lib
---
+++ scripts/package.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+# Install all dependencies
+echo "Installing dependencies for hybridzip ..."
+
+echo "Installing tensorflow-lite ..."
+# Install tflite libraries at /usr/lib
+sh ./install_tflite.sh "/usr/lib"
+
+# Compile hybridzip
+cd ..
+
+cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release . && make hybridzip
+
+
+# Package all dependencies ...
+echo "Packaging all dependencies ..."
+
+cp /usr/lib/libtensorflowlite.so ./lib/libtensorflowlite.so
+cp /usr/lib/libtensorflowlite_gpu_delegate.so ./lib/libtensorflowlite_gpu_delegate.so
+
+# Compress package
+echo "Compressing package ..."
+
+mkdir package
+mv ./bin ./package/bin
+mv ./lib ./package/lib
+cp ./scripts/install.sh ./package/install.sh
+
+tar -zcvf package.tar.gz package
+
+echo "Completed packaging successfully ..."
+
